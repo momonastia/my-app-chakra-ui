c0f3c0b3f03b6b4483a876e9e0bb3c576262b8c1
momonastia~ <anastasiatyrikova@gmail.com>
2023-04-05 11:38:04 +0200

    Feat: heading and text added

--- src/App.js
+++ src/App.js
@@ -8,6 +8,9 @@ import {
   Code,
   Grid,
   theme,
+  Container,
+  Heading,
+  Divider,
 } from '@chakra-ui/react';
 import { ColorModeSwitcher } from './components/ColorModeSwitcher';
 import { Logo } from './Logo';
@@ -17,6 +20,13 @@ function App() {
   return (
     <ChakraProvider theme={theme}>
       <Header />
+      <Container maxWidth="container.lg">
+        <Box>
+          <Heading size="2xl">Chakra UI test Page</Heading>
+          <Text fontSize="xl">Hello from Chakra</Text>
+        </Box>
+      </Container>
+      <Divider></Divider>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
